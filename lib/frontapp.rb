f77370bbf555c3fe262251146fd542c9d2caba35
Niels van der Zanden <niels@phusion.nl>
2017-02-08 09:16:05 +0100

    Do not monkeypatch OpenSSL::SSL::VERIFY_PEER

--- lib/frontapp.rb
+++ lib/frontapp.rb
@@ -3,6 +3,3 @@ require_relative 'frontapp/client.rb'
 require 'openssl'
 require 'http'
 require 'json'
-
-OpenSSL::SSL.send :remove_const, :VERIFY_PEER
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
